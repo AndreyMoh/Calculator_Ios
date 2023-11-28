5203195b0d6ff92933f4544d02eaf534f543a238
AndreyMoh <117999885+AndreyMoh@users.noreply.github.com>
2023-11-28 23:29:00 +0300

    Adding_Big_Numbers_And_Fix_Base_Functions

--- TestApp3/app/src/main/java/com/example/testapp3/MainActivity.java
+++ TestApp3/app/src/main/java/com/example/testapp3/MainActivity.java
@@ -9,6 +9,8 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
 
@@ -17,7 +19,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private float b;
     private float res;
 
-    private float numb = 0;
+    private String numb = "";
 
     private boolean isActionClicked;
     Action action = Action.NULLACTION;
@@ -27,7 +29,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-         resTextView = findViewById(R.id.res_textview);
+        resTextView = findViewById(R.id.res_textview);
 
         final Button one_sign_button = findViewById(R.id.one_sign_button);
         final Button two_sign_button = findViewById(R.id.two_sign_button);
@@ -81,57 +83,69 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.one_sign_button) {
-            resTextView.setText(R.string.one_sign);
-            numb = 1;
+            numb += "1";
+            resTextView.setText(numb);
         } else if (v.getId() == R.id.two_sign_button) {
-            resTextView.setText(R.string.two_sign);
-            numb = 2;
+            numb += "2";
+            resTextView.setText(numb);
         } else if (v.getId() == R.id.three_sign_button) {
-            resTextView.setText(R.string.three_sign);
-            numb = 3;
+            numb += "3";
+            resTextView.setText(numb);
         } else if (v.getId() == R.id.fore_sign_button) {
-            resTextView.setText(R.string.fore_sign);
-            numb = 4;
+            numb += "4";
+            resTextView.setText(numb);
         } else if (v.getId() == R.id.five_sign_button) {
-            resTextView.setText(R.string.five_sign);
-            numb = 5;
+            numb += "5";
+            resTextView.setText(numb);
         } else if (v.getId() == R.id.six_sign_button) {
-            resTextView.setText(R.string.six_sign);
-            numb = 6;
+            numb += "6";
+            resTextView.setText(numb);
         } else if (v.getId() == R.id.seven_sign_button) {
-            resTextView.setText(R.string.seven_sign);
-            numb = 7;
+            numb += "7";
+            resTextView.setText(numb);
         } else if (v.getId() == R.id.eight_sign_button) {
-            resTextView.setText(R.string.eight_sign);
-            numb = 8;
+            numb += "8";
+            resTextView.setText(numb);
         } else if (v.getId() == R.id.nine_sign_button) {
-            resTextView.setText(R.string.nine_sign);
-            numb = 9;
+            numb += "9";
+            resTextView.setText(numb);
         } else if (v.getId() == R.id.zero_sign_button) {
-            resTextView.setText(R.string.zero_sign);
-            numb = 0;
+            numb += "0";
+            resTextView.setText(numb);
         } else if (v.getId() == R.id.subtract_button) {
+            checkAction();
             isActionClicked = true;
             action = Action.SUBTRACT;
+            numb = "";
         } else if (v.getId() == R.id.divide_button) {
+            checkAction();
             isActionClicked = true;
             action = Action.DIVIDE;
+            numb = "";
         } else if (v.getId() == R.id.multiple_button) {
+            checkAction();
             isActionClicked = true;
             action = Action.MULTIPLE;
+            numb = "";
         } else if (v.getId() == R.id.sum_button) {
+            checkAction();
             isActionClicked = true;
             action = Action.SUM;
+            numb = "";
         } else if (v.getId() == R.id.percent_button) {
             action = Action.PERCENT;
-            numb *= 0.01;
+            res *= 0.01;
+            a = res;
+            numb = String.valueOf(res);
             resTextView.setText("" + numb);
         } else if (v.getId() == R.id.dot_button) {
             isActionClicked = true;
             action = Action.DOTFUNC;
         } else if (v.getId() == R.id.plus_minus_button) {
             action = Action.PLUS_MINUS;
-            numb *= -1;
+            res *= -1;
+            a = res;
+            numb = String.valueOf(res);
             resTextView.setText("" + numb);
         } else if (v.getId() == R.id.equal_button) {
             equal(action);
@@ -139,16 +153,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             resTextView.setText(R.string.zero_sign);
             action = Action.NULLACTION;
             isActionClicked = false;
+            numb = "";
             a = 0;
             b = 0;
             res = 0;
         }
 
-        if (!isActionClicked) a = numb;
-        else b = numb;
         Toast.makeText(getApplicationContext(), "a, b = " + a + " " + b, Toast.LENGTH_SHORT).show();
     }
 
+    private void checkAction() {
+        if (action == Action.NULLACTION) {
+            if (!Objects.equals(numb, ""))
+                if (res != 0)
+                    a = res;
+                else a = Float.parseFloat(numb);
+        } else {
+            if (!Objects.equals(numb, ""))
+                b = Float.parseFloat(numb);
+        }
+    }
     private enum Action {
         SUBTRACT,
         SUM,
@@ -169,6 +193,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private float multiple(float a, float b) { return (float) a * b; }
 
     private void equal(Action action) {
+        checkAction();
+
         switch (action) {
             case SUBTRACT:
                 res = subtract(a, b);
@@ -185,6 +211,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case NULLACTION:
                 break;
         }
+        a = res;
         resTextView.setText("" + res);
     }
 }
